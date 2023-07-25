ea65613d720d2c7160b6b38c65c80aecb40f03e7
Your Name <you@example.com>
2023-07-25 12:02:07 +0700

    baru

--- BARU/lib/constan.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Palete {
-  static Color b1 = Color.fromRGBO(224, 105, 7, 1);
-  static Color b2 = Color.fromRGBO(2, 11, 90, 1);
-  static Color b3 = Color(0xfff7892b);
-}
